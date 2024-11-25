4a848d13f352bf59b21b06474fde13b465fc8cc5
AJBen2005s <144455909+AJBen2005s@users.noreply.github.com>
2024-11-24 22:42:43 -0400

    Final

---
+++ server.js
@@ -0,0 +1,37 @@
+//
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+
+
+//
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Import routes
+const usersRoute = require('./routes/users');
+const productsRoute = require('./routes/products');
+
+// Middleware to parse JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Allows parsing of URL-encoded data
+
+//
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }, // Set to true if using HTTPS
+}));
+
+// Serve images from the public/images folder
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+// Use the routes
+app.use('/users', usersRoute);
+app.use('/products', productsRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
